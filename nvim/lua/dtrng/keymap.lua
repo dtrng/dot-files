97439578fb065bd2f3f80415747a44d5721605e8
David Trång <dtrang@baffinbaynetworks.com>
2026-02-26 11:43:16 +0100

    update plugins

--- nvim/lua/dtrng/keymap.lua
+++ nvim/lua/dtrng/keymap.lua
@@ -3,10 +3,14 @@
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save buffer" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -14,7 +18,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -26,10 +30,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -38,27 +42,27 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 --
 
-vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { desc = 'Vertical split' })
-vim.keymap.set('n', '<leader>h', ':split<CR>', { desc = 'Vertical split' })
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<leader>h", ":split<CR>", { desc = "Vertical split" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', '<C-w>z', function()
-  local current_tab = vim.api.nvim_get_current_tabpage()
+vim.keymap.set("n", "<C-w>z", function()
+	local current_tab = vim.api.nvim_get_current_tabpage()
 
-  -- If we're in a zoom tab, go back
-  if vim.t.zoom_origin then
-    local origin = vim.t.zoom_origin
-    vim.t.zoom_origin = nil
-    vim.cmd 'tabclose'
-    vim.api.nvim_set_current_tabpage(origin)
-    return
-  end
+	-- If we're in a zoom tab, go back
+	if vim.t.zoom_origin then
+		local origin = vim.t.zoom_origin
+		vim.t.zoom_origin = nil
+		vim.cmd("tabclose")
+		vim.api.nvim_set_current_tabpage(origin)
+		return
+	end
 
-  -- Otherwise create a zoom tab
-  local origin_tab = current_tab
-  vim.cmd 'tab split'
+	-- Otherwise create a zoom tab
+	local origin_tab = current_tab
+	vim.cmd("tab split")
 
-  -- Mark this tab as a zoom of the previous one
-  vim.t.zoom_origin = origin_tab
+	-- Mark this tab as a zoom of the previous one
+	vim.t.zoom_origin = origin_tab
 end)
